582d9f281ad7ce2fb8b15a6f7ea4456d88494faf
lucianoarendt <30930778+lucianoarendt@users.noreply.github.com>
2018-10-12 18:27:31 -0300

    Teste de leitura de arquivo

---
+++ Teste de leitura de arquivo/leitura.c
@@ -0,0 +1,22 @@
+int main()
+{
+    printf("Digite o nome do arquivo: \n");
+    scanf("%s", nome_arq);
+    FILE* fil = fopen(nome_arq, "r");
+
+    if(fil == NULL)
+        printf("Deu ruim!");
+    else
+    {
+        int tamanhoDaMatriz;
+        fscanf(fil,"%i", &tamanhoDaMatriz);
+
+        matriz =(float*)malloc(tamanhoDaMatriz*sizeof(float));
+        incognitas = (char*)malloc(tamanhoDaMatriz*sizeof(char));
+
+
+
+
+    }
+    return 0;
+}
